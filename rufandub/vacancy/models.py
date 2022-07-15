9be154361176610a456d465479adb14a537feeee
Shaan <shaan-kun@yandex.ru>
2022-07-15 16:37:25 +0400

    настройка админки: добавление моделей, псевдонимов и фильтров

--- rufandub/vacancy/models.py
+++ rufandub/vacancy/models.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 
 
 class Vacancy(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    image = models.ImageField(upload_to="images/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(blank=True, verbose_name="Текст вакансии")
+    image = models.ImageField(upload_to="images/%Y/%m/%d/", verbose_name="Изображение")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категории")
 
     def __str__(self):
         return self.title
@@ -17,12 +17,22 @@ class Vacancy(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+        ordering = ['time_create', 'title']
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'category_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
