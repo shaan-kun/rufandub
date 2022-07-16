3f5a8ddaede59da96b9e739a6197fbeed57bdab9
Shaan <shaan-kun@yandex.ru>
2022-07-16 17:07:02 +0400

    форма добавления статей, связанная с моделью

--- rufandub/vacancy/views.py
+++ rufandub/vacancy/views.py
@@ -28,15 +28,12 @@ def about(request):
 
 def add_page(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
 
         if form.is_valid():
             # print(form.cleaned_data)
-            try:
-                Vacancy.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
 
