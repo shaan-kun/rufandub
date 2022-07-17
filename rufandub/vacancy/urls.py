9655b83594b57632f8bdcce36f8162f5912b8f32
Shaan <shaan-kun@yandex.ru>
2022-07-17 12:23:01 +0400

    Добавлены классы представлений

--- rufandub/vacancy/urls.py
+++ rufandub/vacancy/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', VacancyHome.as_view(), name='home'),
     path('about/', about, name='about'),
-    path('add_page/', add_page, name='add_page'),
+    path('add_page/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
-    path('post/<slug:post_slug>', show_post, name='post'),
-    path('category/<slug:category_slug>', show_category, name='category'),
+    path('post/<slug:post_slug>', ShowPost.as_view(), name='post'),
+    path('category/<slug:category_slug>', VacancyCategory.as_view(), name='category'),
 ]
--- rufandub/vacancy/views.py
+++ rufandub/vacancy/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
 from .forms import *
 from .models import *
@@ -11,33 +13,65 @@ menu = [
     {'title': 'Войти', 'url_name': 'login'},
 ]
 
-def index(request):
-    posts = Vacancy.objects.filter(is_published=True)
+class VacancyHome(ListView):
+    model = Vacancy
+    template_name = 'vacancy/index.html'
+    context_object_name = 'posts'
 
-    context = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Главная страница',
-        'selected_category': 0,
-    }
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-    return render(request, 'vacancy/index.html', context=context)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['selected_category'] = 0
+
+        return context
+
+    def get_queryset(self):
+        return Vacancy.objects.filter(is_published=True)
+
+# def index(request):
+#     posts = Vacancy.objects.filter(is_published=True)
+#
+#     context = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Главная страница',
+#         'selected_category': 0,
+#     }
+#
+#     return render(request, 'vacancy/index.html', context=context)
 
 def about(request):
     return render(request, 'vacancy/about.html', {'menu': menu, 'title': 'О сайте'})
 
-def add_page(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
 
-        if form.is_valid():
-            # print(form.cleaned_data)
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'vacancy/add_page.html'
+    success_url = reverse_lazy('home')
 
-    return render(request, 'vacancy/add_page.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['menu'] = menu
+        context['title'] = 'Добавление статьи'
+
+        return context
+
+
+# def add_page(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#
+#     return render(request, 'vacancy/add_page.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 def contact(request):
     return HttpResponse("Обратная связь")
@@ -45,32 +79,66 @@ def contact(request):
 def login(request):
     return HttpResponse("Авторизация")
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Vacancy, slug=post_slug)
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Vacancy, slug=post_slug)
+#
+#     context = {
+#         'post': post,
+#         'menu': menu,
+#         'title': post.title,
+#         'selected_category': post.category_id,
+#     }
+#
+#     return render(request, 'vacancy/post.html', context=context)
 
-    context = {
-        'post': post,
-        'menu': menu,
-        'title': post.title,
-        'selected_category': post.category_id,
-    }
 
-    return render(request, 'vacancy/post.html', context=context)
+class ShowPost(DetailView):
+    model = Vacancy
+    template_name = 'vacancy/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
 
-def show_category(request, category_slug):
-    posts = Vacancy.objects.filter(category__slug=category_slug)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-    if len(posts) == 0:
-        raise Http404()
+        context['menu'] = menu
+        context['title'] = context['post']
 
-    context = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Отображение по рубрикам',
-        'selected_category': category_slug,
-    }
+        return context
 
-    return render(request, 'vacancy/index.html', context=context)
+
+class VacancyCategory(ListView):
+    model = Vacancy
+    template_name = 'vacancy/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Vacancy.objects.filter(category__slug=self.kwargs['category_slug'], is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['menu'] = menu
+        context['title'] = 'Категория — ' + str(context['posts'][0].category)
+        context['selected_category'] = context['posts'][0].category_id
+
+        return context
+
+# def show_category(request, category_slug):
+#     posts = Vacancy.objects.filter(category__slug=category_slug)
+#
+#     if len(posts) == 0:
+#         raise Http404()
+#
+#     context = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Отображение по рубрикам',
+#         'selected_category': category_slug,
+#     }
+#
+#     return render(request, 'vacancy/index.html', context=context)
 
 
 def pageNotFound(request, exception):
