fdc95e45b44721f321d0b550247b8502edc8e543
Shaan <shaan-kun@yandex.ru>
2022-07-17 21:16:42 +0400

    Добавлена регистрация

--- rufandub/vacancy/utils.py
+++ rufandub/vacancy/utils.py
@@ -7,7 +7,6 @@ menu = [
     {'title': 'О сайте', 'url_name': 'about'},
     {'title': 'Добавить статью', 'url_name': 'add_page'},
     {'title': 'Обратная связь', 'url_name': 'contact'},
-    {'title': 'Войти', 'url_name': 'login'},
 ]
 
 
--- rufandub/vacancy/views.py
+++ rufandub/vacancy/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -130,6 +132,18 @@ class VacancyCategory(DataMixin, ListView):
 #     }
 #
 #     return render(request, 'vacancy/index.html', context=context)
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'vacancy/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+
+        return dict(list(context.items()) + list(c_def.items()))
 
 
 def pageNotFound(request, exception):
